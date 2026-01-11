22320cda9cba7f4cc5f55add8c866ec11c2c960d
sanjaybrammadandi <sanjaybrammadandi@gmail.com>
2026-01-11 17:04:00 +0530

    push

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>       
+        <div>
+          <h1>hi welcome to react</h1>
+          <h2>this is my first react class</h2>
+        </div>
+    </>
+
+  )
+}
+
+export default App
